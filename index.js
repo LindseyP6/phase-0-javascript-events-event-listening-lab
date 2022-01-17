5d704586660a8d16d8ef3aac5ad07c4aad6f4a35
Lindsey <lindsey.pregent@gmail.com>
2022-01-17 15:06:51 -0500

    commit tree 8d532048f7c0b4d65ace8d4e8543390b50c6f4a1

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+// function addingEventListener() {
+//     alert('I was clicked!');
+// }
+
+// const input = document.getElementById('input');
+// input.addEventListener('click', addingEventListener)
+
+const input = document.querySelector('input');
+
 function addingEventListener() {
+  input.addEventListener('click', function(event) {
+    return "clicked"
+  });
 }
